09a00dffafe10da930082b64f9f294fc052d4577
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-09 20:22:46 +0800

    Create Day2.java
    NurulAnnisa

---
+++ Day2.java
@@ -0,0 +1,18 @@
+public class Day2 {
+
+    
+    public static void main(String[] args) {
+        System.out.println(" Hallo Word ");
+        System.out.print (" Hallo Word ");
+        
+        String nama=" Nurul Annisa";
+        int umur=17;
+        String alamat= "Rangas";
+        
+        System.out.printf(" Nama%s, umur:%d, alamat:%s",nama,umur,alamat);
+        
+        
+      
+    }
+    
+}
