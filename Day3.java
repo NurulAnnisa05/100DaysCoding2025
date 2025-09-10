b825037849d3fc1825b9a293b78282bf0ff52f94
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-10 17:59:54 +0800

    Create Day3.java
    Nurul Annisa

---
+++ Day3.java
@@ -0,0 +1,12 @@
+public class Day3 {
+
+    
+    public static void main(String[] args) {
+        //komentar untuk satu baris
+        
+        /*
+        ini contoh komentar multiline atau komentar yang menulis catatan lebih dari satu baris
+        */
+    }
+    
+}
