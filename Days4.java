ffbb81866f08afbe46dc522305145ccbdf70c5cb
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-11 23:19:19 +0800

    Create Days4.java
    Nurul Annisa

---
+++ Days4.java
@@ -0,0 +1,17 @@
+public class Day4 {
+
+    
+    public static void main(String[] args) {
+        System.out.println(" Hallo\nSelamat datang ");
+        System.out.print (" Nama\tNurul ");
+        System.out.print (" Saya berkata\"ada sesuatu yang bagus\" ");
+        
+        // \n:newline(baris baru)
+       //  \t:tab (tabulasi)
+      //  \": double quote 
+        
+        
+      
+    }
+    
+}
