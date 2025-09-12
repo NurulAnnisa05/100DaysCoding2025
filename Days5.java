57fbbe46e0ab7dd7fa88c8540d6750c572cc035e
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-12 20:06:55 +0800

    Create Days5.java
    Nurul

---
+++ Days5.java
@@ -0,0 +1,22 @@
+public class Day5 {
+
+    
+    public static void main(String[] args) {
+        // menyimpan usia
+        byte usia=17;
+        System.out.println("usia saya:"+ usia);
+        //suatu komunitas
+        short jumlahAnggota=2000;
+        System.out.println(" jumlah komunitas:"+ jumlahAnggota);
+        //menyimpan harga produk
+        int hargaSatuanApel=2000;
+        int jumlahProduk =3;
+        int totalHarga;
+        System.out.println("harga satuan+ jumlah produak : + total harga" ); 
+        //jarak rata-rata rumah ke sekolah 
+        long jarakRumahSekolah=100;
+        System.out.println(" jarak rumah ke sekolah: + jarak rumah ke sekolah Km ");
+        
+    }
+    
+}
