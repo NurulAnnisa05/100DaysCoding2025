f60b50b733c6a32574a6cfb0c88a881cc7e6d475
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-13 21:35:56 +0800

    Create Days6.java

---
+++ Days6.java
@@ -0,0 +1,14 @@
+public class Day6 {
+
+    
+    public static void main(String[] args) {
+     //menggunakan tipe data float  
+      float tinggiBadan=153.7f;
+        System.out.println("tinggi badan :" + tinggiBadan);
+        
+        //menggunakan tipe data double
+        double beratBadan=41.7;
+        System.out.println("berat badan:" + beratBadan);
+    }
+    
+}
