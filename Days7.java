4ca242f88a5889296a0a6b9de75fc3582c88e92e
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-14 16:29:14 +0800

    Create Days7.java
    Nurul

---
+++ Days7.java
@@ -0,0 +1,12 @@
+public class Day7 {
+
+    
+    public static void main(String[] args) {
+    char   nilaiSaya='A';
+     System.out.println(" Nilai saya:"+nilaiSaya);
+
+     boolean sayaPeringkat=trus;
+     System.out.println(" saya peringkat:" + sayaPeringkat);
+    }
+    
+}
