92034b4a21994c57dfc1a52710830718762090bd
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-15 23:33:39 +0800

    Create Days8.java
    Nurul

---
+++ Days8.java
@@ -0,0 +1,11 @@
+public class Day8 {
+
+    
+    public static void main(String[] args) {
+   String nama=" Nurul";
+   String alamat="rangas";
+     System.out.println(" nama saya adalah" + nama);
+System.out.println("alamat"+ alamat);
+    }
+    
+}
