e97268867c821bc8d007e6813f109a1d84ffb0fb
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-16 18:05:06 +0800

    Create Days9.java
    nurul

---
+++ Days9.java
@@ -0,0 +1,22 @@
+public class Day9 {
+
+    
+    public static void main(String[] args) {
+   //Deklarasi 
+   String nama;
+   int umur;
+
+   //Inisialisasi
+   nama="galif";
+   umur=18;
+
+   //update 
+   umur=17;
+   nama="nurul";
+
+      
+System.out.println(nama); //Output: Nurul 
+System.out.println(umur); //Output: 26
+    }
+    
+}
