b3cf8f3a4ab144690e1b0f33001231009e754e2e
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-17 23:51:36 +0800

    Create Days10.java
    Nurul

---
+++ Days10.java
@@ -0,0 +1,9 @@
+public class Day10 {
+
+    
+    public static void main(String[] args) { 
+      final double tinggiBadan= 153.7;
+  System.out.println(tinggi badan :" + tinggiBadan ); 
+    }
+    
+}
