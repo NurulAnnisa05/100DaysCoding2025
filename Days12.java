ebc9fae02e890c3976b0613262c3617ad36e6aa4
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-19 23:38:12 +0800

    Create Days12.java
    Nurul

---
+++ Days12.java
@@ -0,0 +1,21 @@
+public class Day12 {
+
+    
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        //masukkan input
+        System.out.println("nama:");
+        String nama=scanner.nextLine();
+        System.out.println("umur: ");
+        int umur=scanner.nextInt();
+        System.out.println("berat Badan:");
+        double beratBadan=scanner.nextDouble();
+       //menampilkan biodata lengkap
+        System.out.println("nama:"+nama);
+        System.out.println(" umur:"+ umur);
+        System.out.println("berat badan:"+ beratBadan);
+        
+        
+    }
+    
+}
