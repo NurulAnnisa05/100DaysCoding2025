9facc600f7ec39af4d3d3224a06e4c25db16c8c7
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-21 22:47:53 +0800

    Create Days014.java
    Nurul

---
+++ Days014.java
@@ -0,0 +1,16 @@
+public class Day14 {
+
+        public static void main(String[] args) {
+        String stringAngka="123";
+        int integerAngka=Integer.parseInt(stringAngka); 
+            System.out.println("Tipe data: int, nilai:"+ integerAngka);
+        String stringDesimal="2.008";
+        double doubleAngka=Double.parseDouble(stringDesimal);
+            System.out.println(" Tipe data: double, nilai:" + doubleAngka);
+        String stringFalse="False";
+        boolean booleanValue=Boolean.parseBoolean(stringFalse);
+            System.out.println(" Tipe data: boolean, nilai:" + booleanValue);
+    }
+
+    
+    }
