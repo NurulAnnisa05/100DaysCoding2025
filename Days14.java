95a2b5e3e9fbf5c238878c00ceb5f3a01da8ec76
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-21 22:10:23 +0800

    Create Days14.java
    Nurul

---
+++ Days14.java
@@ -0,0 +1,12 @@
+public class Day14 {
+
+        public static void main(String[] args) {
+        //mengubah string menjadi integer
+        String stringAngka;
+        int integerAngka=17;
+            System.out.println("Tipe data:int,nilai:"+ integerAngka);
+            
+    }
+
+    
+    }
