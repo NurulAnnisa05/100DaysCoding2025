670aeab1ec8147411bcac6dbb6300f77d378e83f
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-22 17:51:20 +0800

    Create Days15.java
    Nurul

---
+++ Days15.java
@@ -0,0 +1,15 @@
+public class Day15 {
+
+        public static void main(String[] args) {
+
+        int angka1 = 26;
+        int angka2 = 13;
+        int hasilTambah = angka1 + angka2;
+     System.out.println("Hasil penjumlahan dari " + angka1 + " + " + angka2 + " adalah: " + hasilTambah);
+        
+        double angka3 = 13.5;
+        double angka4 = 26.1;
+        double hasilKurang = angka3 - angka4;
+        System.out.println("Hasil pengurangan dari " + angka3 + " - " + angka4 + " adalah: " + hasilKurang); 
+    }
+}
