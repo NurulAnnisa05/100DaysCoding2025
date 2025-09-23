596c3b663c296f95724e9aac5503e7d5220c8591
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-23 21:23:19 +0800

    Create Days16.java
    Nurul

---
+++ Days16.java
@@ -0,0 +1,16 @@
+public class Day16 {
+
+    
+    public static void main(String[] args) {
+      int angka1=1;
+      int angka2=5;
+      int hasilPerkalian=angka1*angka2;
+        System.out.println(" hasil perkalian 1*5="+hasilPerkalian);
+        
+      int angka3=23;
+      int angka4=3;
+      int hasilPembagian=angka3/angka4;
+        System.out.println("hasil pembagian 23/3="+hasilPembagian);
+    }
+    
+}
