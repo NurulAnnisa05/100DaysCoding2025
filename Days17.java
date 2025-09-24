f97d8a1597b27d8fb6b7e0ce22f5eb6df2244109
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-24 17:18:20 +0800

    Create Days17.java
    Nurul

---
+++ Days17.java
@@ -0,0 +1,10 @@
+public class Day17 {
+    public static void main(String[] args) {
+
+        int a = 13;
+        int b = 5;
+        int sisaBagi = a % b;
+
+    System.out.println("Hasil dari 13 % 5 adalah: " + sisaBagi); 
+    }
+}
