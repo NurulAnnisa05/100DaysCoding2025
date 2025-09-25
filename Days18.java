eb37511c0d0f28a63b1acd9818be21276f32691f
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-25 22:45:43 +0800

    Create Days18.java
    Nurul

---
+++ Days18.java
@@ -0,0 +1,10 @@
+public class Day18 {
+    public static void main(String[] args) {
+
+        int angkaInt = 261;
+        int angkaLong = angkaInt;
+System.out.println(" nilai int: " + angkaInt); 
+
+    System.out.println(" nilai long: " + angkaLong); 
+    }
+}
