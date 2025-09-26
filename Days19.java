f15ac939004088d9d90977006f7a6eabd81d3ed8
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-26 22:01:13 +0800

    Create Days19.java
    Nurul

---
+++ Days19.java
@@ -0,0 +1,10 @@
+public class Day19 {
+    public static void main(String[] args) {
+
+       double nilaiDoubleBesar  = 153.7;
+System.out.println(" nilai awal(double): " + nilaiDoubleBesar); 
+
+ int angkaIntKecil=(int)nilaiDoubleBesar;
+    System.out.println(" hasil konversi double ke int: " + angkaIntKecil); 
+    }
+}
