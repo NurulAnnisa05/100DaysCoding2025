111cfdd8967ba2f5c1ec60c578968cb30db76b5e
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-27 23:46:12 +0800

    Create Days20.java
    Nurul

---
+++ Days20.java
@@ -0,0 +1,14 @@
+public class Day20 {
+    public static void main(String[] args) {
+        int angkaInt = 12;
+        double angkaDouble = 12.3;
+        boolean nilaiBoolean = true;
+
+        String strInt = String.valueOf(angkaInt);
+        String strDouble = String.valueOf(angkaDouble);
+        String strBoolean = String.valueOf(nilaiBoolean);
+    System.out.println("Integer ke String: " + strInt);
+        System.out.println("Double ke String: " + strDouble);
+        System.out.println("Boolean ke String: " + strBoolean);
+    }
+}
