e29190f82a0ebe362478db13c5d6bba2ee01bde8
nurllannisaa01-arch <nurllannisaa01@gmail.com>
2025-09-28 22:44:20 +0800

    Create Day21.java
    Nurul

---
+++ Day21.java
@@ -0,0 +1,14 @@
+public class Day21 {
+    public static void main(String[] args) {
+        int a = 26;
+        int b = 13;
+        
+        System.out.println("Sebelum ditukar: a = " + a + ", b = " + b);
+        
+       
+        a = 26;        
+        b = 13;
+        
+        System.out.println("Setelah ditukar: a = " + b + ", b = " + a);
+    }
+}
