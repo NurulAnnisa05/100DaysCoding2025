c23e81a16b4a728d5a7aa17c5270c8fdc5d30ecb
NurulAnnisa05 <nurullannisaamukramin@gmail.com>
2025-10-10 22:05:19 +0800

    Create Days33.java
    Nurul

---
+++ Days33.java
@@ -0,0 +1,15 @@
+public class Day33 {
+    public static void main(String[] args) {
+        
+        boolean punyaSIM = true;
+        
+      
+        boolean tidakPunyaSIM = !punyaSIM; 
+        
+             System.out.println("Anda punya SIM (true): " + punyaSIM);
+        System.out.println("Anda TIDAK punya SIM (!true): " + tidakPunyaSIM); 
+
+       
+        
+    }
+}
