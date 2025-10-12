04fc64ae6660c97e0f395fbd3a5f2702e831cb62
NurulAnnisa05 <nurullannisaamukramin@gmail.com>
2025-10-12 22:31:25 +0800

    Create Days35.java
    Nurul

---
+++ Days35.java
@@ -0,0 +1,13 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int nilai = 75;
+
+       
+        if (nilai > 70) {
+            
+            System.out.println("Selamat! Anda lulus.");
+        }
+
+        System.out.println("Program selesai.");
+    }
+}
