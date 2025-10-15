81ae9cf0ecd95f75e1221e18245b898ed04ee4ed
NurulAnnisa05 <nurullannisaamukramin@gmail.com>
2025-10-15 22:36:41 +0800

    Create Days38.java
    Nurul

---
+++ Days38.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Day38 {
+    
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int bilangan;
+        System.out.println("masukkan sebuah bilangan bulat:");
+        bilangan=input.nextInt();
+        if (bilangan >0) {
+            System.out.println("bilangan"+bilangan + "adalah bilangan positif.");
+        } else if (bilangan >0) {
+            System.out.println("bilangan"+bilangan +"adalah bilangan negatif.");
+        } else{ 
+            System.out.println("bilangan"+bilangan+"adalah nol");
+            input.close();
+        }
+                    
+   
+}
+            
+        }
