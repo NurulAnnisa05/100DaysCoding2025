3ac10d8eb0ac3880e88778906c6bedc4b4a71710
NurulAnnisa05 <nurullannisaamukramin@gmail.com>
2025-10-20 22:23:39 +0800

    Update Days43.java
    Nurul

--- Days43.java
+++ Days43.java
@@ -1,27 +1,35 @@
-public class Day43 {
+public class Day43{
     public static void main(String[] args) {
+        
         int batasAtas = 15;
+        int i = 1;
 
-        System.out.println("--- Program Cek Kelipatan 3, 5, dan Keduanya (1 sampai " + batasAtas + ") ---");
+        System.out.println("--- Program Cek Kelipatan 3, 5, dan Keduanya (Menggunakan WHILE) ---");
+
         
-        
-        for (int i = 1; i <= batasAtas; i++) {
+        while (i <= batasAtas) {
             System.out.print("Angka " + i + ": ");
 
-  
+           
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("Kelipatan 3 dan 5 (FizzBuzz)");
-            } 
+            }
+          
             else if (i % 3 == 0) {
                 System.out.println("Kelipatan 3 (Fizz)");
-            } 
+            }
+            
             else if (i % 5 == 0) {
                 System.out.println("Kelipatan 5 (Buzz)");
-            } 
+            }
+            
             else {
                 System.out.println("Bukan kelipatan 3 atau 5");
             }
+
+           
+            i++;
         }
-        System.out.println("-------------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------");
     }
 }
