905790c8543b1d01fa80496e003a14b480d70645
NurulAnnisa05 <nurullannisaamukramin@gmail.com>
2025-10-26 22:05:43 +0800

    Create Days49.java
    .

---
+++ Days49.java
@@ -0,0 +1,17 @@
+public class Day49 {
+    public static void main(String[] args) {
+        int angka = 7;
+        String hasil;
+
+         nilai_jika_salah;
+        hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+
+        System.out.println("Angka " + angka + " adalah: " + hasil);
+        
+        
+        int angka2 = 10;
+        String hasil2 = (angka2 % 2 == 0) ? "Genap" : "Ganjil";
+        
+        System.out.println("Angka " + angka2 + " adalah: " + hasil2);
+    }
+}
