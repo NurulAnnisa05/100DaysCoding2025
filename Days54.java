7035064d0739a9602daa3ae3f2d329b6eaeadbd5
NurulAnnisa05 <nurullannisaamukramin@gmail.com>
2025-10-31 21:55:56 +0800

    Create Days54.java
    .

---
+++ Days54.java
@@ -0,0 +1,15 @@
+public class Day54{
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5; i++) {
+            
+            
+            if (i == 3) {
+                
+            }
+            
+       
+            System.out.println("Angka: " + i);
+        }
+    }
+}
